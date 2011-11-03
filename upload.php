031fc02de1742ce1fec9476ab2bc5718e1f90e7b
Justin Parton <justin.parton@gmail.com>
2011-11-03 15:24:29 -0500

    added comments

--- upload.php
+++ upload.php
@@ -1,21 +1,24 @@
 <?php
 include 'vimeo.php';
 
-$vimeo = new phpVimeo('CONSUMER_KEY', 'CONSUMER_SECRET', 'ACCESS_TOKEN', 'ACCESS_TOKEN_SECRET');
+//DATE CONFIGURATION
+//---------------------------------------------
+$today = date("D M j Y");
+date_default_timezone_set('America/Chicago');
+//---------------------------------------------
+
+$vimeo = new phpVimeo('bb5b982d90d89cf2dfce78317eb63446', 'd620a5c1ea8734d4', '5121c29d3aac80c0691935e849bab97f', '4dfd3de08016923d81de17a6a2c735742f623e2b');
 
 try {
-	
-	//Recall that the video file is relative to the CMD current directory so:
-	//   i call the file like this C:\php uploader/upload.php... 
-	//   the file must be in C:\
-    $video_id = $vimeo->upload('PATH_TO_VIDEO_FILE');
+	//------ location of video file must be relative to Cmd.exe current working directory
+    $video_id = $vimeo->upload('ready.mp4');
 
     if ($video_id) {
         echo '<a href="http://vimeo.com/' . $video_id . '">Upload successful!</a>';
 
-        //$vimeo->call('vimeo.videos.setPrivacy', array('privacy' => 'nobody', 'video_id' => $video_id));
-        $vimeo->call('vimeo.videos.setTitle', array('title' => 'YOUR TITLE', 'video_id' => $video_id));
-        $vimeo->call('vimeo.videos.setDescription', array('description' => 'YOUR_DESCRIPTION', 'video_id' => $video_id));
+        //$vimeo->call('vimeo.videos.setPrivacy', array('privacy' => 'anyone', 'video_id' => $video_id));
+        $vimeo->call('vimeo.videos.setTitle', array('title' => 'GBFIC:' . $today, 'video_id' => $video_id));
+        $vimeo->call('vimeo.videos.setDescription', array('description' => $today, 'video_id' => $video_id));
     }
     else {
         echo "Video file did not exist!";
